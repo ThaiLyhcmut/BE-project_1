f7c3e4c6aa12c5b1ae2a8b81ecffd140533384dd
Thaily <lyvinhthai321@gmail.com>
2024-09-06 23:10:33 +0700

    0.0.01

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express")
 const app = express()
 const port = 3000;
 
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+
+
 app.get("/", (req, res) => {
-  res.send("Trang chu")
+  res.render("client/pages/home/index")
 })
 
+app.get("/products", (req, res) => {
+  res.render("client/pages/products/index")
+})
 
 app.listen(port, () => {
   console.log(`website đang chạy localhot: http://localhost:${port}`)
