cc70108e382f623f3f77c7e9d3c5734c07658d3e
Thaily <lyvinhthai321@gmail.com>
2024-09-07 21:56:46 +0700

    0.0.03

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const routeClient = require("./routes/client/index_route");
 const app = express()
-const port = 3000;
+const port = process.env.PORT;
 
 app.set('views', './views')
 app.set('view engine', 'pug')
